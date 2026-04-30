99a5458d0feca3d8d38912d3b4f00a04e693188c
Jawwad Umar <jawwadumar99@gmail.com>
2026-04-30 09:52:55 +0530

    login route done

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -42,8 +42,44 @@ router.post("/signup", async(request, response) =>{
 })
 
 //LoginRoute
-// router.post("/login", async(request, response)=>{
-//     //login logic
-// })
+router.post("/login", async(request, response)=>{
+    //login logic [email and password]
+    try{
+      const {email, password} = request.body;
+
+      if (!email || !password){
+        return response.status(400).json({
+          message: "Email and password are required",
+        });
+      }
+
+      //Check the user with given email
+      const user = await User.findOne({email});
+
+      if(!user){
+        return response.status(404).json({
+          message: "User with this email does not exists",
+        })
+      };
+
+      //Check if this combination of email and password exists in the database
+      const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+      if (!isPasswordMatch){
+        return response.status(401).json({
+          message: "Incorrect password",
+        })
+      }
+
+      response.status(200).json({
+        message:"User sign in successfully",
+        email: user.email,
+      });
+    }
+    catch(error){
+      console.log("Signin Error: ", error);
+        return response.status(500).json({message: "Something went wrong"});
+    }
+})
 
 export default router;
